7611dfa0bd14b50469d09d40cb537519e0b281f0
Austin Blackwell <austinbobs@gmail.com>
2021-06-17 18:40:27 -0700

    June17

--- postDojo/June/CommonCharacterCount.js
+++ postDojo/June/CommonCharacterCount.js
@@ -30,6 +30,45 @@ Guaranteed constraints:
 */
 
 function commonCharacterCount(s1, s2) {
+    count = 0;
+    //create a counting variable 
+    arr = s1.split('');
+    //Use split function to turn items in single items in an array
+    // console.log(arr)
+    arr1 = s2.split('');
+    //Use split function to turn items in single items in an array
+    // console.log(arr1)
+    for (i = 0; i < s1.length; i++) {
+        //Loop through first array
+        for (j = 0; j < s2.length; j++) {
+            //loop through second array
+            // console.log(arr[i], arr1[j])
+            if (arr[i] == arr1[j]) {
+                //If the iterated items in both arrays are the same
+                count++;
+                //increase count
+                // console.log(count)
+                arr1.splice(j, 1);
+                //Use the splice function to remove the item just compared
+                // console.log(arr1)
+                break;
+                //add the break to stop an additional count
+            }
+        }
+    } return count;
+}
+
+console.log(commonCharacterCount("aabcc", "adcaa"), "Should be 3");
+console.log(commonCharacterCount("zzzz", "zzzzzzz"), "Should be 4");
+console.log(commonCharacterCount("abca", "xyzbac"), "Should be 3");
+console.log(commonCharacterCount("a", "b"), "Should be 0");
+console.log(commonCharacterCount("a", "aaa"), "Should be 1");
+
+
+//THIS WAS MY ORIGINAL ATTEMPT
+//VERY SIMILAR TO ABOVE SOLUTION
+/*
+function commonCharacterCount(s1, s2) {
     var count = 0;
     var array1 = s1.split('')
     console.log(array1)
@@ -50,7 +89,8 @@ function commonCharacterCount(s1, s2) {
 }
 
 console.log(commonCharacterCount("aabcc", "adcaa"));
-// console.log(commonCharacterCount());
-// console.log(commonCharacterCount());
-// console.log(commonCharacterCount());
-// console.log(commonCharacterCount());
+console.log(commonCharacterCount("zzzz", "zzzzzzz"));
+console.log(commonCharacterCount("abca", "xyzbac"));
+console.log(commonCharacterCount("a", "b"));
+console.log(commonCharacterCount("a", "aaa"));
+*/
