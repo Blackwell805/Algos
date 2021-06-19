f90a13b7bb0e65007da122a3f16e93b01ec14235
Austin Blackwell <austinbobs@gmail.com>
2021-06-19 14:02:03 -0700

    Added csharp algo folder

---
+++ csharp/June/ReturnNegative.cs
@@ -0,0 +1,29 @@
+/*
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Example:
+
+Kata.MakeNegative(1); // return -1
+Kata.MakeNegative(-5); // return -5
+Kata.MakeNegative(0); // return 0
+Notes:
+
+The number can be negative already, in which case no change is required.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+*/
+
+
+namespace csharp.June
+{
+    public class ReturnNegative
+    {
+        public static int MakeNegative(int number)
+        {
+            if (number > 0)
+                number *= -1;
+            return number;
+        }
+    }
+}
+// console.writeline(MakeNegative(42));
+
